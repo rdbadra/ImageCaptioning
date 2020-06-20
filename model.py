bf41965707b0a0c968b56d78d3255e631121662b
roberto <rdbadra@gmail.com>
2020-06-20 15:12:23 +0200

    commented model.py

--- model.py
+++ model.py
@@ -5,8 +5,14 @@ from torch.nn.utils.rnn import pack_padded_sequence
 
 
 class FeatureExtractor(nn.Module):
+    """ Class used to build the CNN model. """
     def __init__(self, embedding_dim):
-        """Load the pretrained ResNet-34 and replace top fc layer."""
+        """ Constructor of the CNN model
+        
+        Args:
+            embedding_dim: int
+                Size of the embedding. Equivalent to the length of the CNN output.
+        """
         super(FeatureExtractor, self).__init__()
         resnet = models.resnet34(pretrained=True)
         modules = list(resnet.children())[:-1]      # delete the last fc layer.
@@ -15,7 +21,12 @@ class FeatureExtractor(nn.Module):
         self.bn = nn.BatchNorm1d(embedding_dim, momentum=0.01)
         
     def forward(self, images):
-        """Extract feature vectors from input images."""
+        """Extract feature vectors from input images and normalize data
+        
+        Args:
+            images: Tensor
+                Batch of images to predict.
+        """
         with torch.no_grad():
             features = self.resnet(images)
         features = self.linear(features.reshape(features.size(0), -1))
@@ -24,15 +35,36 @@ class FeatureExtractor(nn.Module):
         return features
 
 class CaptionGenerator(nn.Module):
+    """ Class to build the LSTM model. """
     def __init__(self, embedding_dim, hidden_size, vocab_size, num_layers):
-        """Set the hyper-parameters and build the layers."""
+        """ Constructor of the LSTM model.
+        
+        Args:
+            embedding_dim: int
+                Size of the embedding. Equivalent to the CNN output.
+            hidden_size: int
+                Size of the LSTM.
+            vocab_size: int
+                Size of the vocabulary.
+            num_layers: int
+                Number of layers of the LSTM.
+
+        """
         super(CaptionGenerator, self).__init__()
         self.embed = nn.Embedding(vocab_size, embedding_dim)
         self.lstm = nn.LSTM(embedding_dim, hidden_size, num_layers, batch_first=True)
         self.linear = nn.Linear(hidden_size, vocab_size)
         
     def forward(self, features, captions):
-        """Decode image feature vectors and generates captions."""
+        """ Decode image feature vectors and generates captions. 
+        
+        Args:
+            features: Tensor
+            captions: Tensor
+
+        Returns:
+            outputs: Tensor
+        """
         captions = captions[:, :-1]
         embeddings = self.embed(captions)
         embeddings = torch.cat((features.unsqueeze(1), embeddings), dim=1)
@@ -40,10 +72,22 @@ class CaptionGenerator(nn.Module):
         outputs = self.linear(lstm_out)
         return outputs
 
-    def sample(self, features, states=None, max_len=20):
-        """Generate captions for an image"""
+    def sample(self, features, max_len=20):
+        """ Generate captions for an image.
+        
+        Args:
+            features: Tensor
+                Feature vector of the CNN output.
+            max_len: int
+                Maximum length of generated caption.
+
+        Returns:
+            caption: Tensor
+                Generated caption.
+        """
         caption = []
         inputs = features.unsqueeze(1)
+        states = None
         for i in range(max_len):
             hiddens, states = self.lstm(inputs, states)
             outputs = self.linear(hiddens.squeeze(1))
